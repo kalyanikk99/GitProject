82755e4849c2789179d91598c2c596c896670774
kalyanikk99 <kalyanikk99@gmail.com>
2016-02-26 21:32:26 +0530

    add family our too baby..

---
+++ GitProject/src/com/ninescript/gitProject/HelloFamily.java
@@ -0,0 +1,11 @@
+package com.ninescript.gitProject;
+
+public class HelloFamily 
+{
+	public void showMe()
+	{
+		System.out.println("Hello Aai and Baba... ");
+	}
+
+
+}
--- GitProject/src/com/ninescript/gitProject/HelloKK.java
+++ GitProject/src/com/ninescript/gitProject/HelloKK.java
@@ -5,6 +5,9 @@ public class HelloKK
 
 	
 	public static void main(String[] args) {
+		HelloFamily family= new HelloFamily();
+		family.showMe();
+		
 		System.out.println("hello kalyani....!!!");
 		System.out.println("hello shridhar....");
 		System.out.println("hello kedar....");
